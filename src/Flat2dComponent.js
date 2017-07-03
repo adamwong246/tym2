a57ec3849beb8efad000207fd517159b399c244a
adam wong <adamwong246@gmail.com>
2017-07-03 13:25:08 -0700

    update

--- src/Flat2dComponent.js
+++ src/Flat2dComponent.js
@@ -2,6 +2,8 @@ import moment from 'moment'
 
 import {tym2Time, svgHeight, svgWidth} from './data.js'
 
+const buffer = 5;
+
 export default function Flat2dComponent(props) {
 	const rowHeight = svgHeight / props.events.length;
 
@@ -15,12 +17,12 @@ export default function Flat2dComponent(props) {
 					props.events.map(function(lmnt, ndx){
 						return (
 							<g>
-							  <rect x={yScale(lmnt.start)} y={ndx*rowHeight} height={rowHeight} width={yScale(lmnt.end) - yScale(lmnt.start)} fillOpacity={props.highlighted === lmnt.id ? 1 : 0.1} fill="pink" stroke="black" onMouseOver={(e) => props.onHighlight(lmnt.id)} ></rect>
+							  <rect x={yScale(lmnt.start)} y={ndx*rowHeight+buffer} height={rowHeight-buffer} width={yScale(lmnt.end) - yScale(lmnt.start)} fillOpacity={props.highlighted === lmnt.id ? 1 : 0.1} fill="pink" stroke="black" onMouseOver={(e) => props.onHighlight(lmnt.id)} ></rect>
 							</g>
 						)
 					})
 				}
-    <line x1={yScale(moment())} y1="0" x2={yScale(moment())} y2={svgHeight} stroke="red"/>
+    <line x1={yScale(moment())} y1="0" x2={yScale(moment())} y2={svgHeight} stroke="red" strokeOpacity="0.5" strokeWidth="3"/>
     <rect x="0" y="0" width={svgWidth} height={svgHeight} stroke="red" strokeWidth="2" fill="transparent"/>
 			</svg>
 		</div>
--- src/Fold2dComponent.js
+++ src/Fold2dComponent.js
@@ -2,6 +2,7 @@ import moment from 'moment'
 
 import {tym2Time, stratifiedDB, svgHeight, svgWidth} from './data.js'
 const yScale = tym2Time.range([0, svgWidth]);
+const buffer = 5;
 
 function Fold2dRecursiveComponent(props) {
   const node = props.root.data
@@ -10,7 +11,7 @@ function Fold2dRecursiveComponent(props) {
 
   return (
     <g>
-     <rect x={yScale(node.start)} y={ndx*height} height={height} width={yScale(node.end) - yScale(node.start)} fill="pink" fillOpacity={props.highlight ? 1 : 0.1} stroke="black" onMouseOver={(e) => props.onHighlight(props.root.data.id)}></rect>
+     <rect x={yScale(node.start)} y={ndx*height+buffer} height={height-buffer} width={yScale(node.end) - yScale(node.start)} fill="pink" fillOpacity={props.highlight ? 1 : 0.1} stroke="black" onMouseOver={(e) => props.onHighlight(props.root.data.id)}></rect>
     <g>{
      (props.root.children || []).map(function(lmnt, ndx2){
       return(<Fold2dRecursiveComponent root={lmnt} ndx={ndx2} height={height / props.root.children.length} highlighted={props.highlighted} highlight={props.highlighted === lmnt.data.id} onHighlight={props.onHighlight} />)
@@ -27,8 +28,8 @@ export default function Fold2dComponent(props) {
 			<svg height={svgHeight} width={svgWidth}>
     <rect x="0" y="0" width={svgWidth} height={svgHeight} fill="lightgray"/>
 			 <Fold2dRecursiveComponent root={stratifiedDB} height={svgHeight} highlighted={props.highlighted} onHighlight={props.onHighlight}/>
-    <line x1={yScale(moment())} y1="0" x2={yScale(moment())} y2={svgHeight} stroke="red"/>
-    <rect x="0" y="0" width={svgWidth} height={svgHeight} stroke="red" strokeWidth="2px" fill="transparent"/>
+    <line x1={yScale(moment())} y1="0" x2={yScale(moment())} y2={svgHeight} stroke="red" strokeOpacity="0.5" strokeWidth="3"/>
+    <rect x="0" y="0" width={svgWidth} height={svgHeight} stroke="red" strokeWidth="2px" fill="transparent" />
 			</svg>
 		</div>
 	);
